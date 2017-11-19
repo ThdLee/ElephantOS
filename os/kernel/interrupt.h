cfed141ee24200e022c806079d045c6b72f3f292
thdlee <leejy2333@outlook.com>
2017-11-19 23:23:22 +0800

    add makefile, debug, interrupt and timer

---
+++ os/kernel/interrupt.h
@@ -0,0 +1,16 @@
+#ifndef __KERNEL_INTERRUPT_H
+#define __KERNEL_INTERRUPT_H
+#include "stdint.h"
+typedef void* intr_handler;
+void idt_init(void);
+
+enum intr_status {
+	INTR_OFF,		// 中断关闭
+	INTR_ON			// 中断打开
+};
+
+enum intr_status intr_get_status(void);
+enum intr_status intr_set_status(enum intr_status);
+enum intr_status intr_enable(void);
+enum intr_status intr_disable(void);
+#endif 
